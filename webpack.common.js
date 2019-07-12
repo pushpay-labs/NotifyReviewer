40a1d8be0afe426ef690661bf07984a6895873a2
erwan-joly <erwan.joly@pushpay.com>
2019-07-12 15:50:30 +1200

    remove useless loader

--- webpack.common.js
+++ webpack.common.js
@@ -35,16 +35,6 @@ module.exports = {
         }, {
           loader: 'css-loader', // translates CSS into CommonJS modules
         }, {
-          loader: 'postcss-loader', // Run post css actions
-          options: {
-            plugins: function () { // post css plugins, can be exported to postcss.config.js
-              return [
-                require('precss'),
-                require('autoprefixer')
-              ];
-            }
-          }
-        }, {
           loader: 'sass-loader' // compiles Sass to CSS
         }]
       },
